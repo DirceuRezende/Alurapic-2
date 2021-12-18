3cf9063da7c26bae0772f5ddf02fc813721b5743
Dirceu Rezende <dirceu.srezende@gmail.com>
2021-12-18 16:38:28 -0300

    Create Painel Component and change App

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,69 @@
 <template>
-  <div id="app">
-    <h1>{{ titulo }}</h1>
-    <img :src="foto.url" :alt="foto.titulo" />
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto in fotos" :key="foto.titulo">
+        <meu-painel :titulo="foto.titulo">
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+        </meu-painel>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue';
+
 export default {
   name: 'app',
+    components: {
+     'meu-painel': Painel
+  },
   data () {
       return {
         titulo: 'Alurapic',
-        foto: {
-          url: 'https://matsudapet.com.br/blog/wp-content/uploads/2019/08/shutterstock_559799125-compressed.jpg',
-          titulo: 'Cachorro'
-      }
+        fotos: [
+          {
+            url: 'https://matsudapet.com.br/blog/wp-content/uploads/2019/08/shutterstock_559799125-compressed.jpg',
+            titulo: 'Cachorro 1'
+          },
+          {
+            url: 'https://matsudapet.com.br/blog/wp-content/uploads/2019/08/shutterstock_559799125-compressed.jpg',
+            titulo: 'Cachorro 2'
+          }
+        ]
     }
+  },
+
+  created() {
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos)
+      .catch(err => console.log(err));
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .centralizado {
+    text-align: center;
+  }
 
-h1, h2 {
-  font-weight: normal;
-}
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  .lista-fotos {
+    list-style: none;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
 
-a {
-  color: #42b983;
-}
+  .imagem-responsiva {
+    width: 100%;
+  }
 </style>
